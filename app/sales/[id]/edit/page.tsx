d17f40d337944fadc2b4a94bd5c8b81297111264
samaziz679 <toutankaziz@yahoo.fr>
2025-08-08 03:15:05 +0000

    Update page.tsx

--- app/sales/[id]/edit/page.tsx
+++ app/sales/[id]/edit/page.tsx
@@ -17,6 +17,7 @@ export default async function EditSalePage({ params }: { params: { id: string } 
     notFound();
   }
 
+  
   return (
     <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
       <Breadcrumb>
